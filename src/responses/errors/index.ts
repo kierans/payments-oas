2bf5177869144d170072e2e31aeba602f23a207c
Kieran Simpson <kierans777@gmail.com>
2023-06-08 22:22:14 +1000

    Add internal-server error schema

--- src/responses/errors/index.ts
+++ src/responses/errors/index.ts
@@ -1,12 +1,14 @@
 import { NamedResponses } from "../../../types/oas";
 
 import badRequest from "./bad-request";
+import internalServer from "./internal-server";
 import notFound from "./not-found";
 import unauthorised from "./unauthorised";
 import unprocessable from "./unprocessable";
 
 export default (): NamedResponses => ({
 	"bad-request": badRequest(),
+	"internal-server": internalServer(),
 	"not-found": notFound(),
 	"unauthorised": unauthorised(),
 	"unprocessable": unprocessable()
