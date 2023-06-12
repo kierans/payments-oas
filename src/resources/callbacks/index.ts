00938a02c97ace2469936273a4cf7ef916d830f3
Kieran Simpson <kierans777@gmail.com>
2023-06-12 13:10:35 +1000

    Add callback for payments.

--- src/resources/callbacks/index.ts
+++ src/resources/callbacks/index.ts
@@ -3,9 +3,45 @@ import PathsObject = OpenAPIV3_1.PathsObject;
 import PathItemObject = OpenAPIV3_1.PathItemObject;
 import { parameterReference, responsesReference } from "../../lib";
 
+const paymentCallback: () => PathItemObject = () => ({
+	post: {
+		operationId: "onPayment",
+		summary: "Payment Callback",
+		description: "Receive updates from a payments provider about a payment",
+		tags: [
+			"callbacks"
+		],
+		security: [
+			{}
+		],
+		parameters: [
+			parameterReference("provider")
+		],
+		requestBody: {
+			content: {
+				"application/json": {
+					schema: {
+						type: "object",
+						title: "Anything",
+						additionalProperties: true
+					}
+				}
+			}
+		},
+		responses: {
+			"204": {
+				description: "If the event is received and processed",
+				content: {}
+			},
+			"400": responsesReference("bad-request"),
+			"500": responsesReference("internal-server")
+		}
+	}
+})
+
 const paymentAgreementCallback: () => PathItemObject = () => ({
 	post: {
-		operationId: "onPaymentAgreementCallback",
+		operationId: "onPaymentAgreement",
 		summary: "Payment Agreement Callback",
 		description: "Receive updates from a payments provider about an agreement",
 		tags: [
@@ -40,5 +76,6 @@ const paymentAgreementCallback: () => PathItemObject = () => ({
 })
 
 export default (): PathsObject => ({
-	"/callbacks/{provider}/paymentagreements": paymentAgreementCallback()
+	"/callbacks/{provider}/payment": paymentCallback(),
+	"/callbacks/{provider}/paymentagreement": paymentAgreementCallback()
 })
