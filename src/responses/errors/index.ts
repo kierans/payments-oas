f9a75d98fa9e56ebfcd726b64090a231336903d8
Kieran Simpson <kierans777@gmail.com>
2023-06-28 12:26:22 +0000

    Remove unneeded error.

--- src/responses/errors/index.ts
+++ src/responses/errors/index.ts
@@ -5,13 +5,11 @@ import internalServer from "./internal-server";
 import notFound from "./not-found";
 import paymentFailure from "./payment-failure";
 import unauthorised from "./unauthorised";
-import unprocessable from "./unprocessable";
 
 export default (): NamedResponses => ({
 	"bad-request": badRequest(),
 	"internal-server": internalServer(),
 	"not-found": notFound(),
 	"payment-failure": paymentFailure(),
-	"unauthorised": unauthorised(),
-	"unprocessable": unprocessable()
+	"unauthorised": unauthorised()
 })
--- src/responses/errors/unprocessable.ts
+++
@@ -1,6 +0,0 @@
-import { OpenAPIV3_1 } from "openapi-types";
-import ResponseObject = OpenAPIV3_1.ResponseObject;
-
-import { errors } from "./errors";
-
-export default (): ResponseObject => errors("The provided ID is not known")
